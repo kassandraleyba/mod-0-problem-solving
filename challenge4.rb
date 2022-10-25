82eef482d1e0a793cdc328e3eaeb1bc1b828d4e0
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-25 05:46:01 -0600

    Add challenge 4 changes

--- challenge4.rb
+++ challenge4.rb
@@ -1,5 +1,6 @@
 # Start with an array of strings. Print only the words that include the letter combination "ing".
 
+# Make an array that lists only words that end in "ing".
 # Step 1. Create an array of strings
 # Step 2. Use a method to print all words in the array
 # Step 3. Use a method and an argument to print only words that end in "ing"
