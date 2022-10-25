bb9deb101807b4b22a40ae6d9911ef5dc5e2fbca
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-25 06:07:30 -0600

    Add partial challenge6

---
+++ challenge6.rb
@@ -0,0 +1,8 @@
+# Given a sentence with only lowercase letters, print the same sentence with the first letter of 
+# every word capitalized. For example, if you were given "Turing is the best", return "Turing Is 
+# The Best" instead!
+
+# Make an all lowercase word sentence and print a new sentence with each first letter capitalized.
+# Step 1: Create a string with all lowercase variable.
+# Step 2: Add a method and an argument to capitalize the first letter of each word
+
