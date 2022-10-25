fa309e4c92968ffda66d1d45ea56c9f8f8268e5a
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-25 05:23:40 -0600

    Add challenge4

---
+++ challenge4.rb
@@ -0,0 +1,13 @@
+# Start with an array of strings. Print only the words that include the letter combination "ing".
+
+# Step 1. Create an array of strings
+# Step 2. Use a method to print all words in the array
+# Step 3. Use a method and an argument to print only words that end in "ing"
+
+verbs = ["climbing", "run", "eating", "lift", "coloring", "looking"]
+
+verbs.each do |verb|
+    if verb.include?("ing")
+        puts verb
+    end
+end
