9616425014061ced9f79edafa8ef0db505b2afb8
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-25 19:01:09 -0600

    Add challenge 6

--- challenge6.rb
+++ challenge6.rb
@@ -6,3 +6,25 @@
 # Step 1: Create a string with all lowercase variable.
 # Step 2: Add a method and an argument to capitalize the first letter of each word
 
+string = ("Turing is the best")
+
+# def capitalize_first_letter(s)
+#     words = s.split #transforming & reassigning, splitting into an array
+#     capitalize_words = [] #declaring an empty array
+#     words.each do |word|
+#         capitalize_words << word.capitalize
+#     end
+#     capitalize_words.join(" ")
+# end
+
+# puts capitalize_first_letter(string)
+
+def capitalize_first_letter(s)
+    words = s.split 
+    capitalize_words = words.map do |word|
+        word.capitalize
+    end
+    capitalize_words.join(" ")
+end
+
+puts capitalize_first_letter(string)
